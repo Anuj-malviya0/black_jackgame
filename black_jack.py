bc649b385c5ff49d1a71358ae680c1dc6a41bd69
Anuj Malviya <75200824+Anuj-malviya0@users.noreply.github.com>
2021-02-12 21:27:38 +0530

    added more classes

--- black_jack.py
+++ black_jack.py
@@ -34,3 +34,47 @@ class Deck:
     def deal(self):
         single_card = self.deck.pop()
         return single_card
+
+test_deck = Deck()
+print(test_deck)
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+    
+    def adjust_for_ace(self):
+        pass
+test_deck = Deck()
+test_deck.shuffle()
+test_player = Hand()
+test_player.add_card(test_deck.deal())
+test_player.add_card(test_deck.deal())
+test_player.value
+
+
+
+for card in test_player.cards:
+    print(card)
+
+class Hand:
+    
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1  # add to self.aces
+    
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1 
