60a2c725510a08ee90c8a35eecb17b21fa3bce50
Anuj Malviya <75200824+Anuj-malviya0@users.noreply.github.com>
2021-02-18 20:25:46 +0530

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -78,3 +78,27 @@ class Hand:
         while self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1 
+class Chips:
+    
+    def __init__(self):
+        self.total = 100  # This can be set to a default value or supplied by a user input
+        self.bet = 0
+        
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -= self.bet
+
+def take_bet(chips):
+    
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Sorry, a bet must be an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
