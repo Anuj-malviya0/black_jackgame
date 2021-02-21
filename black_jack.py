1688d3f2dd430f409c23e46309b499fc671dcab3
Anuj Malviya <75200824+Anuj-malviya0@users.noreply.github.com>
2021-02-21 21:08:32 +0530

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -1,19 +1,22 @@
+#importing random module 
 import random
-
+#creating necessary lists and dictonaries 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
 
+#A boolean  variable wich will help later to run the game 
 playing = True
+#A class for creating a card object whh will have it's suit and rank
 class Card:
 
     def __init__(self,suit,rank):
         self.suit = suit
-        self.rank = rank
-        
+        self.rank = rank       
     def __str__(self):
         return self.rank + ' of ' + self.suit
+#A class for creating a deck object which will have 52 cards,and it will use card object 
 class Deck:
     
     def __init__(self):
@@ -34,33 +37,9 @@ class Deck:
     def deal(self):
         single_card = self.deck.pop()
         return single_card
-
 test_deck = Deck()
 print(test_deck)
-class Hand:
-    def __init__(self):
-        self.cards = []  # start with an empty list as we did in the Deck class
-        self.value = 0   # start with zero value
-        self.aces = 0    # add an attribute to keep track of aces
-    
-    def add_card(self,card):
-        self.cards.append(card)
-        self.value += values[card.rank]
-    
-    def adjust_for_ace(self):
-        pass
-test_deck = Deck()
-test_deck.shuffle()
-test_player = Hand()
-test_player.add_card(test_deck.deal())
-test_player.add_card(test_deck.deal())
-test_player.value
-
-
-
-for card in test_player.cards:
-    print(card)
-
+#A hand object for storing a cards that player will have in it's hand 
 class Hand:
     
     def __init__(self):
@@ -78,6 +57,19 @@ class Hand:
         while self.value > 21 and self.aces:
             self.value -= 10
             self.aces -= 1 
+
+#testing the above code with a test deck
+test_deck = Deck()
+test_deck.shuffle()
+test_player = Hand()
+test_player.add_card(test_deck.deal())
+test_player.add_card(test_deck.deal())
+test_player.value
+
+
+for card in test_player.cards:
+    print(card)
+
 class Chips:
     
     def __init__(self):
